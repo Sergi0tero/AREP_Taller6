27d382c160ccc059abd7551c60e8c28e7f65b31e
unknown <sergio.otero@labinfo.is.escuelaing.edu.co>
2023-03-09 15:29:04 -0500

    RoundRobin funciona

--- src/main/java/RoundRobin.java
+++ src/main/java/RoundRobin.java
@@ -1,15 +1,36 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.Spark.port;
 
+
 public class RoundRobin {
+    private static MongoClient client = null;
+    private static String url = "localhost:27017";
+    private static MongoDatabase db = null;
+    private static MongoCollection<Document> collec;
+
+    public static void createConnection(){
+        client = new MongoClient(url);
+        db = client.getDatabase("admin");
+        collec = db.getCollection("database1");
+
+    }
     public static void main(String[] args) {
+        createConnection();
         port(getPort());
-        get("/prueba", (request, response) -> {
-            System.out.println("entro");
+        get("/logs", (request, response) -> {
             URL url = new URL("http://localhost:4088/logs");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -23,12 +44,36 @@ public class RoundRobin {
                 while (scanner.hasNext()) {
                     responseBody += scanner.nextLine();
                 }
-                System.out.println(responseBody);
                 scanner.close();
             }
-
             conn.disconnect();
-            return "";
+            return responseBody;
+        });
+        post("/logs", (request, response) -> {
+            URL url = new URL("http://localhost:4088/logs");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "text/plain");
+
+            String requestBody = request.body();
+            byte[] postData = requestBody.getBytes("UTF-8");
+            con.setDoOutput(true);
+            OutputStream os = con.getOutputStream();
+            os.write(postData);
+            os.flush();
+            os.close();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer responseBody = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                responseBody.append(inputLine);
+            }
+            in.close();
+
+            con.disconnect();
+
+            return responseBody.toString();
         });
     }
 
